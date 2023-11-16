ac53fded54bb88f62a356b2270774a97444f59b6
Florian Georg <florian.alexander.georg@gmail.com>
2023-11-16 14:19:57 +0100

    added frontpage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const heroStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    background: 'linear-gradient(to right, #6dd5ed, #2193b0)',
+    color: 'white',
+    fontSize: '2rem',
+    fontFamily: 'Arial, sans-serif',
+    textAlign: 'center',
+    padding: '20px'
+  };
+
+  return (
+    <div style={heroStyle}>
+      <h1>Welcome to the SDSC GenAI Hackathon</h1>
+    </div>
+  );
 }
 
 export default App;
